e6efb9974243b2df60b97b140741d30891cfa141
RileyLeff <rileyleff@gmail.com>
2023-05-01 14:59:00 -0400

    styled files n shit

--- R/model_init.R
+++ R/model_init.R
@@ -1,12 +1,9 @@
-
-
-
-new_sperry_model = function(){
-    xyz = new("Sperry_Model")
-    xyz@data = read.csv(system.file("extdata", "ex_data_short.csv", package = "ezsperry"))
-    xyz@data$Jmax_optional = as.numeric(xyz@data$Jmax_optional)
-    xyz@data$Vmax_optional = as.numeric(xyz@data$Vmax_optional)
-    xyz@data$Lai_optional = as.numeric(xyz@data$Lai_optional)
-    xyz@Parameters@gs_limits = read.csv(system.file("extdata", "ex_gs_limits.csv", package = "ezsperry"))
-    return(xyz)
+new_sperry_model <- function() {
+  xyz <- new("Sperry_Model")
+  xyz@data <- read.csv(system.file("extdata", "ex_data_short.csv", package = "ezsperry"))
+  xyz@data$Jmax_optional <- as.numeric(xyz@data$Jmax_optional)
+  xyz@data$Vmax_optional <- as.numeric(xyz@data$Vmax_optional)
+  xyz@data$Lai_optional <- as.numeric(xyz@data$Lai_optional)
+  xyz@Parameters@gs_limits <- read.csv(system.file("extdata", "ex_gs_limits.csv", package = "ezsperry"))
+  return(xyz)
 }
--- tests/testthat/test_please.R
+++ tests/testthat/test_please.R
@@ -1,5 +1,5 @@
 test_that("tests could theoretically work if I had them", {
-    x <- 1
-    actual <- 1
-    expect_identical(actual, x)
+  x <- 1
+  actual <- 1
+  expect_identical(actual, x)
 })
